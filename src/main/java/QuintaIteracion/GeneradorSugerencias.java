915ec170324e1fc5682e4a1525c307efe761ad67
delficostanzo <delfi.costanzo@hotmail.com>
2021-05-26 11:15:54 -0300

    Agrego lo que estaba en la cuarta iteracion a la quinta iteracion

---
+++ src/main/java/QuintaIteracion/GeneradorSugerencias.java
@@ -0,0 +1,12 @@
+package QuintaIteracion;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public interface GeneradorSugerencias {
+    public List<Atuendo> generarSugerenciasDesde(List <Prenda> prendasAptas);
+    //al no haber implementado el QMP3, por recomendacion de franco para implementar la logica de la combinacion de las prendas
+    //para formar una sugerencia utilizamos esta interfaz, en el cual alguna clase la implementa
+}
---
+++ src/main/java/QuintaIteracion/Prenda.java
@@ -0,0 +1,67 @@
+package QuintaIteracion;
+
+import QuintaIteracion.Exceptions.*;
+
+import java.math.BigDecimal;
+
+public class Prenda {
+    private TipoPrenda tipoSeleccionado;
+    private Material materialSeleccionado;
+    private Trama tramaSeleccionada;
+    private Color colorPrincipalSeleccionado;
+    private Color colorSecundarioSeleccionado;
+    BigDecimal temperaturaAdecuada;
+
+
+    public boolean temperaturaValida(BigDecimal temperatura){
+        return temperaturaAdecuada.intValue() < temperatura.intValue() ;
+    }
+
+
+
+    Prenda(TipoPrenda tipoSolicitado, Material materialSolicitado, Trama tramaSolicitada, Color colorPrincipalSolicitado, Color colorSecundarioSolicitado, BigDecimal temperatura) {
+        //como requerimento primero lo que cargamos es el tipo de prenda solicitado:
+        if (tipoSolicitado == null) {
+            throw new TipoException("Debes seleccionar un tipo de atuendo a utilizar");
+        }
+        this.tipoSeleccionado = tipoSolicitado;
+
+
+        if (materialSolicitado == null) {
+            throw new MaterialException("Debes seleccionar un tipo de material para la prenda");
+        }
+        if(!tipoSolicitado.admiteMaterial(materialSolicitado)){
+            throw new TipoException("Debes seleccionar un material consistente con el tipo de prenda");
+        }
+        this.materialSeleccionado = materialSolicitado;
+
+
+
+        //trama
+        if (tramaSolicitada == null) {
+            tramaSolicitada = Trama.LISA;
+        }else {
+            this.tramaSeleccionada = tramaSolicitada;
+        }
+
+
+
+        if (colorPrincipalSolicitado == null) {
+            throw new ColorException("Debes seleccionar un color de prenda");
+        }
+        this.colorPrincipalSeleccionado = colorPrincipalSolicitado;
+        //puede ser null el color secundario, no deberia generar ninguna excepcion
+        this.colorSecundarioSeleccionado = colorSecundarioSolicitado;
+        this.temperaturaAdecuada = temperatura;
+    }
+
+
+    Categoria categoriaDePrenda() {
+        return tipoSeleccionado.getCategoria();
+    }
+
+    //POR AHORA no agrego el metodo de cambiar color secundario ya que que considermanos que el colorSecundario no tendria que mutar, pero en caso de q pudiera no estaria de mas
+
+}
+
+
